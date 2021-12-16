1c03e9f2b9f7a592e457a246f55e9bfa8e3787f8
egos <pelletierber@gmail.com>
2021-12-16 23:06:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import pandas as pd
 
+url = "http://egosgame.net/file.txt"
+r = requests.get(url)
+#r.text
+#r.text.split(";")
+print(pd.Series(r.text.split(";")).value_counts())
+
+
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
