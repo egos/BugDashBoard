4a3a2ab6f401ab5c51e7ce4baaeef5e76a916943
egos <pelletierber@gmail.com>
2021-12-16 23:18:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,19 @@
 import streamlit as st
 import pandas as pd
 import requests
+from bs4 import BeautifulSoup
 
 url = "http://egosgame.net/file.txt"
 r = requests.get(url)
 #r.text
 #r.text.split(";")
 st.write(pd.Series(r.text.split(";")).value_counts())
+
+url = "http://egosgame.net/"
+r = requests.get(url)
+soup = BeautifulSoup(r.text).find_all("a")
+FileList = [item['href']  for item in soup if ".json" in item.get('href')]
+st.write(FileList)
 
 
 st.write("Here's our first attempt at using data to create a table:")
